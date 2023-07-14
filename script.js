ce2cc09f1be9c6fca3878dbbb5e7becad685fd44
Allison Perry <allisonperry@allisons-air.lan>
2023-07-14 12:31:07 -0600

    added css

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-var timerEl = document.getElementById(".timer");
-var mainEL = document.getElementById("#container");
-var secondsLeft = 60;
-var buttonEl = document.getElementById("#button")
-var textAreaEl = document.getElementById("#text-area")
+//var timerEl = document.getElementById(".timer");
+//var mainEL = document.getElementById("#container");
+//var secondsLeft = 60;
+//var buttonEl = document.getElementById("#button")
+//var textAreaEl = document.getElementById("#text-area")
 
-function setTime () {
-    var timerInterval = setInterval(function) {
-        secondsLeft--;
-        timerEl.textContent = secondsLeft; + "Oops! You didn't answer all the questions! Check out your score."
+//function setTime () {
+    //var timerInterval = setInterval(function) {
+        //secondsLeft--;
+        //timerEl.textContent = secondsLeft; + "Oops! You didn't answer all the questions! Check out your score."
 
-        if (secondsLeft === 0 && // questions not answered) {
-            clearInterval(timerInterval);
-            sendMessage();  
-        }
-    }, 1);
-}
+        //if (secondsLeft === 0 && // questions not answered) {
+            //clearInterval(timerInterval);
+            //sendMessage();  
+        //}
+    //}, 1);
+//}
 
-function sendMessage() {
-    timerEl.textContent = "";
-}
+//function sendMessage() {
+    //timerEl.textContent = "";
+//}
 
-setTime();
+//setTime();
 
-buttonEl.addEventListener('click', function (event) {
-    event.preventDefault();
-    textAreaEl.value = "";
-}
+//buttonEl.addEventListener('click', function (event) {
+    //event.preventDefault();
+    //textAreaEl.value = "";
+//}
